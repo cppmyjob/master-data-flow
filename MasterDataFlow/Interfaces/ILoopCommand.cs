b01da5d362a4f2760be1bbde54a8338c3c514356
cppmyjob <cpp.myjob@gmail.com>
2014-06-06 18:38:36 +0400

    Forgotten Files

---
+++ MasterDataFlow/Interfaces/ILoopCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MasterDataFlow.EventLoop;
+
+namespace MasterDataFlow.Interfaces
+{
+    public interface ILoopCommand
+    {
+        //ILoopCommandData Data { get; set; }
+        void Execute(Guid loopId, ILoopCommandData data, EventLoopCallback callback);
+    }
+}
---
+++ MasterDataFlow/Utils/AsyncQueue.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+namespace MasterDataFlow.Utils
+{
+    public class AsyncQueue<T>
+    {
+        private readonly Queue _syncdQ = Queue.Synchronized(new Queue());
+
+        public void Enqueue(T item)
+        {
+            _syncdQ.Enqueue(item);
+        }
+
+        public void EnqueueRange(IEnumerable items)
+        {
+            foreach (var item in items)
+            {
+                _syncdQ.Enqueue(item);
+            }
+        }
+
+        public T Dequeue()
+        {
+            return (T)(_syncdQ.Dequeue());
+        }
+
+        public T Peek()
+        {
+            return (T)(_syncdQ.Peek());
+        }
+
+        public int Count
+        {
+            get { return _syncdQ.Count; }
+        }
+
+        public object SyncRoot
+        {
+            get { return _syncdQ.SyncRoot; }
+        }
+
+    }
+}
