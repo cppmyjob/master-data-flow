fd49688806c5e6a92acffa95949b039dadabb978
cppmyjob <cpp.myjob@gmail.com>
2014-10-13 00:52:33 +0400

    Adding some files for remoting

---
+++ MasterDataFlow/EventLoop/EventLoop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MasterDataFlow.EventLoop
+{
+    // TODO now it's wrapper. but we are planning to create a single thread handler of all events
+    public class EventLoop
+    {
+        public static void QueueEvent(WaitCallback callback)
+        {
+            ThreadPool.QueueUserWorkItem(callback);
+        }
+    }
+}
