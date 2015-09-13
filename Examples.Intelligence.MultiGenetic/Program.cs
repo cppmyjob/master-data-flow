6c613cf8d50e95842bc0f59fbcef6509df313847
cppmyjob <cpp.myjob@gmail.com>
2015-09-13 22:14:24 +0300

    Added MultiGenitic example

---
+++ Examples.Intelligence.MultiGenetic/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Examples.Intelligence.MultiGenetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
