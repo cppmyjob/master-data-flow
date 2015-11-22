ee4556c5270ca1f9a42c37297b03d6e67bacdff9
cppmyjob <cpp.myjob@gmail.com>
2015-11-22 21:37:46 +0300

    Add missing file

---
+++ MasterDataFlow.Intelligence/Neuron/Dna/DnaAtomDefinition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MasterDataFlow.Intelligence.Neuron.Dna
+{
+    public class DnaAtomDefinition
+    {
+        public string AtomData { get; set; }
+        public Type AtomType { get; set; }
+        public int[] Inputs { get; set; }
+        public DnaAxon[] Outputs { get; set; }
+    }
+}
