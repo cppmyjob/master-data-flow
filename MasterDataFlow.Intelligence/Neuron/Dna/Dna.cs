238bbe696c88115f8dfd9c269f0f3a765c293cf7
cppmyjob <cpp.myjob@gmail.com>
2015-11-25 21:48:21 +0300

    Add Missing file

---
+++ MasterDataFlow.Intelligence/Neuron/Dna/Dna.cs
@@ -0,0 +1,22 @@
+﻿using MasterDataFlow.Intelligence.Interfaces;
+using MasterDataFlow.Intelligence.Neuron.Builder;
+using MasterDataFlow.Intelligence.Neuron.Builder.Instructions;
+
+namespace MasterDataFlow.Intelligence.Neuron.Dna
+{
+    public class Dna : DnaInOut, IDnaSerializator
+    {
+        public DnaSection[] Sections { get; set; }
+        public void Serialize(WriteBuffer buffer)
+        {
+            AddSectionsInstruction.Serialize(buffer, this);
+
+            foreach (var section in Sections)
+            {
+                section.Serialize(buffer);
+            }
+
+            buffer.Write(StopInstruction.Code);
+        }
+    }
+}
